f94c38304f7da676d269a8380707258e37502c67
Paul Bergmann <pauljbergmann@gmail.com>
2016-11-02 09:28:58 +0100

    Initial commit

---
+++ tests/TestTwoTest.php
@@ -0,0 +1,20 @@
+<?php
+
+include '../src/TestTwo.php';
+
+class TestTwoTest extends PHPUnit_Framework_TestCase
+{
+    private $test;
+
+    protected function setUp()
+    {
+        $this->test = new TestTwo();
+    }
+
+    public function testAdd()
+    {
+        $result = $this->test->foo('Paul', 'Bergmann');
+
+        $this->assertEquals('Bergmann', $result);
+    }
+}
