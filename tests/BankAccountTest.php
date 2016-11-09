c881c3b62e5c18be01ec7897c8bb24f42ab7ec97
Paul Bergmann <pauljbergmann@gmail.com>
2016-11-09 21:16:08 +0100

    Created a new test

---
+++ tests/BankAccountTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class BankAccountTest extends PHPUnit_Framework_TestCase
+{
+    private $test;
+
+    public function __construct()
+    {
+        $this->test = new TestThree();
+    }
+
+    public function testThis()
+    {
+        //
+    }
+}
